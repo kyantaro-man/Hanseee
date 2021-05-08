2df0c197a228398440e47fceac54fb0976d1958d
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-05-08 13:37:31 +0900

    パスワード再設定メール送信機能

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,19 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Mail\ResetPassword;
+use Illuminate\Support\Facades\Mail;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    /**
+     * パスワード再設定メールを送信する
+     */
+    public function sendPasswordResetNotification($token) {
+        Mail::to($this)->send(new ResetPassword($token));
+    }
 
     /**
      * The attributes that are mass assignable.
