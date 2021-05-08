95e4e9a0c6741adb0e3776b2bca6125544af1ecd
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-05-08 12:42:44 +0900

    ログイン時とログアウト時の出し分け

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
+Route::group(['middleware' => 'auth'], function() {
+  Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+})
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
